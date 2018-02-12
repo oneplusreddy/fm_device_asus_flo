42ef3d70aa7a31d138cdeb1cab4e1bd16ef33f8b
oneplusreddy <33752506+oneplusreddy@users.noreply.github.com>
2018-02-12 16:44:18 +0530

    Update device-common.mk

--- device-common.mk
+++ device-common.mk
@@ -19,15 +19,6 @@
 #
 # Everything in this directory will become public
 
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-LOCAL_KERNEL := device/asus/flo/kernel
-else
-LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
-
-PRODUCT_COPY_FILES := \
-	$(LOCAL_KERNEL):kernel
-
 # This device is xhdpi.  However the platform doesn't
 # currently contain all of the bitmaps at xhdpi density so
 # we do this little trick to fall back to the hdpi version
@@ -61,7 +52,7 @@ PRODUCT_PACKAGES += \
 	lights.msm8960
 
 PRODUCT_PACKAGES += \
-    charger_res_images
+    omni_charger_res_images
 
 # Live Wallpapers
 PRODUCT_PACKAGES += \
